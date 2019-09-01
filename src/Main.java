1c1a799afdbcd0a324c28c049048e017f5111797
grigoriishveps <grigorii.shveps@gmail.com>
2019-09-01 13:26:36 +0300

    3stage

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String com = scanner.nextLine();
         String s = scanner.nextLine();
         int l = scanner.nextInt();
         for(char x : s.toCharArray()){
-            if('a'<=x && x<='z') {
-                int a = (x - 'a' + l) % 26;
-                System.out.print((char) (a + 'a'));
+                int a;
+                if(com.equals("enc"))
+                    a = (x + l) ;
+                else
+                    a = ((x  - l));
+                System.out.print((char) (a ));
             }
-            else
-                System.out.print(x);
         }
-    }
+
 }
